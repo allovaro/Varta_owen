695df6c3d71152624a8640dfeb4696f91d14686d
allovaro <allovaro@gmail.com>
2019-06-11 15:54:39 +0300

    change serial lib

--- SerialClass.py
+++ SerialClass.py
@@ -1,6 +1,6 @@
 from PyQt5 import QtCore
 from PyQt5.QtCore import QObject, QIODevice
-from PyQt5.QtSerialPort import QSerialPort
+import serial
 from PyQt5.QtSerialPort import QSerialPortInfo
 import time
 
@@ -10,7 +10,7 @@ class SerialWorker(QtCore.QObject):
     Класс для создания отдельного потока считывания данных из
     COM порта
     """
-    serial = QSerialPort()
+    serial_port = serial.Serial()
 
     def __init__(self, port_name='COM1', baudrate=9600):
         QtCore.QObject.__init__(self)
@@ -20,22 +20,26 @@ class SerialWorker(QtCore.QObject):
     def task(self):
         while True:
             info = QSerialPortInfo.availablePorts()
-            if not self.serial.isOpen():
+            if self.serial_port.isOpen():
+                data = serial_port.readall()
+                print(data)
+            else:
                 for inf in info:
                     if inf.portName() == self.port_name:
-                        try:
-                            self.serial.setPortName(self.port_name)
-                            self.serial.setBaudRate(self.baudrate)
-                            self.serial.setParity(QSerialPort.NoParity)
-                            self.serial.setDataBits(QSerialPort.Data8)
-                            self.serial.setStopBits(QSerialPort.OneStop)
-                            self.serial.open(QIODevice.ReadWrite)
-                            # self.serial.readyRead.connect(self.on_serial_read)
-                        except IOError:
-                            print("Cannot connect to device on port {}".format(self.port_name))
-            else:
-                data = serial.readall()
+                        print('Inf.portName = {}, config_port_name = {}'.format(inf.portName(), self.port_name))
+                        if not self.serial_port.isOpen():
+                            try:
+                                self.serial_port.port = self.port_name
+                                self.serial_port.baudrate = self.baudrate
+                                self.serial_port.bytesize = serial.EIGHTBITS  # number of bits per bytes
+                                self.serial_port.parity = serial.PARITY_NONE  # set parity check: no parity
+                                self.serial_port.stopbits = serial.STOPBITS_ONE  # number of stop bits
+                                self.serial_port.open()
 
 
-
+                                # self.serial.readyRead.connect(self.on_serial_read)
+                            except IOError:
+                                print("Cannot connect to device on port {}".format(inf.portName()))
+                            except:
+                                print("Something else goes wrong")
             time.sleep(1)

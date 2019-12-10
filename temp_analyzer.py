ae5ff7dc691674cfb80915e163ace42db8a3a729
allovaro <allovaro@gmail.com>
2019-12-10 16:56:40 +0300

    анализатор

--- temp_analyzer.py
+++ temp_analyzer.py
@@ -19,13 +19,23 @@ class Analyzer(object):
             return False
 
     def add_new_value(self, new_value):
-        if new_value == self.get_last_value():
-            if len(self.temperature_memory) < 15:
-                self.temperature_memory.append(new_value)
-            else:
-                self.temperature_memory.pop(0)
-                self.temperature_memory.append(new_value)
+        if self.temperature_memory:
+
+            if new_value == self.get_last_value():
+                if len(self.temperature_memory) < 15:
+                    self.temperature_memory.append(new_value)
+                else:
+                    self.temperature_memory.pop(0)
+                    self.temperature_memory.append(new_value)
+        else:
+            self.temperature_memory.append(new_value)
         # print(len(self.temperature_memory))
 
     def get_last_value(self):
         return self.temperature_memory[-1]
+
+analyz = Analyzer()
+for i in range(78):
+    analyz.add_new_value(i + 3)
+print(analyz.temperature_memory)
+print(len(analyz.temperature_memory))
